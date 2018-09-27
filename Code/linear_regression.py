4f15f70940487f995514f69edaf2e279b0f0a6bd
KariEriksen <eriksen_kari@hotmail.com>
2018-09-27 18:38:20 +0200

    So far

--- Code/linear_regression.py
+++ Code/linear_regression.py
@@ -1,26 +1,58 @@
 import numpy as np
 
 class My_Linear_Regression:
-    def __init__(self, X, method, alpha):
-	self.method = method
-	self.alpha = alpha	
+	def __init__(self, X, alpha):
+		self.method = method
+		self.alpha = alpha	
 
-    def My_OLS(self, method, alpha):
+	def My_OLS(self, X, alpha):
 
-	"""
-	Ordinary Least Square method
- 	
-	###### Variables #######
+		"""
+		Ordinary Least Square method
+	 	
+		###### Variables #######
 
-	method = the selected regression method to use
-	alpha = lambda in Ridge or Lasso regression 
+		method = the selected regression method to use
+		alpha = lambda in Ridge or Lasso regression 
 	
-	######  Method   #######
+		######  Method   #######
 	
-	Solves the OLS ....
-	up to fifth order
+		Solves the OLS ....
+		up to fifth order
 	
-	"""
+		"""
+	
+		# Ordinary Least Square method
+		OLS = LinearRegression()
+		OLS.fit(X_fit,z_n)
+		z_predict = OLS.predict(X_fit)
+
+		diff = z_n - z_predict
+		MSE = 1/(n*n)*(sum(diff*diff))
+		
+		return MSE
+
+	def My_Ridge(self, X, alpha):
+
+		ridge = Ridge(alpha)
+		ridge.fit(X_fit, z_n)
+		z_predict = ridge.predict(X_fit)
+
+		diff = z_n - z_predict
+		MSE = 1/(n*n)*(sum(diff*diff))
+		
+		return MSE
+
+	def My_Lasso(self, X, alpha):
+
+		ridge = Ridge(alpha)
+		ridge.fit(X_fit, z_n)
+		z_predict = ridge.predict(X_fit)
+
+		diff = z_n - z_predict
+		MSE = 1/(n*n)*(sum(diff*diff))
+		
+		return MSE
 
              
 
