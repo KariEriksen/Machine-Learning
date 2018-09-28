4dfeb42499e5fe4d975669eff298feb8a55c9b54
KariEriksen <eriksen_kari@hotmail.com>
2018-09-28 14:07:04 +0200

    Trying to split dataset

--- Code/bootstrap.py
+++ Code/bootstrap.py
@@ -1,11 +1,17 @@
 import numpy as np
 from numpy.random import randint
+from linear_regression import My_Linear_Regression 
 
 class Bootstrap:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, X, B, alpha, split):
+        self.X = X
+	self.B = B
+	self.alpha = alpha
+	self.split = split
+	self.method = method
+	
 
-    def bootstrap(self, B, split):
+    def My_Bootstrap(self):
 
 	"""
 	Bootstrap class
@@ -25,16 +31,46 @@ class Bootstrap:
 	Repeated n times
 	Generates sampling distribution of mean
 	"""
-
+	# Split the data according to the given decimal percentage
+	n = np.size(X,1)   # size of column
+	C = (n**2)*split
+	X_test = np.random.choice(x, C))
 	
+	np.random.shuffle(X)
+	training, test = X[:C,:], X[C:,:]
+	
+	# Do linear regression
+	lr = My_Linear_Regression(X, alpha)
+
+	# Ordinary Least Square method
+	if method == 'OLS':
+		z_predict = lr.My_OLS
+
+	# Ridge regression
+	elif method == 'Ridge':
+		z_predict = lr.My_Ridge
+
+	#Lasso regression
+	elif method == 'Lasso':
+		z_predict = lr.My_Lasso
+
+	else:
+		print('You have forgotten to select method; OLS, Ridge or Lasso.')
+	
+
 	n = len(data)
 	t = zeros(n)
-        	for i in range(n):
+        for i in range(n):
 		sample = data[randint(0,n,n)]
-        		t[i] = mean(sample)
+        	t[i] = mean(sample)
 	
+	# Calculate the mean and standard deviation
 	mean = 1.0/n*(sum(t))  
 	std = 1.0/n*(sum())
+
+	# Calculate the Mean Square Error
+	diff = z - z_predict
+	MSE = 1/(n*n)*(sum(diff*diff))
 	
 	return std, mean
 
