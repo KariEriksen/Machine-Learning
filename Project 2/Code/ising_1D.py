480af7662f36c8c6b88654f89f55661a8c3f6d3f
KariEriksen <eriksen_kari@hotmail.com>
2018-11-02 14:14:59 +0100

    doing all stat calc

--- Project 2/Code/ising_1D.py
+++ Project 2/Code/ising_1D.py
@@ -7,10 +7,13 @@ from bootstrap import Bootstrap
 import matplotlib.pyplot as plt
 
 # decide which method to use
-#method = 1 # OLS
-#method = 2 #Ridge
-#method = 3 #Lasso
-method = 4 #Bootstrap with Lasso
+#part = 1 #OLS
+#part = 2 #Ridge
+#part = 3 #Lasso
+#part = 4 #R score with bootstrap
+#part = 5 #MSE with bootstrap
+
+part = sys.argv[1]
 
 # define Ising model aprams
 # system size
@@ -57,7 +60,7 @@ Y_train=Data[1][:n_samples]              #+ np.random.normal(0,4.0,size=X_train.
 X_test=Data[0][n_samples:3*n_samples//2]
 Y_test=Data[1][n_samples:3*n_samples//2] #+ np.random.normal(0,4.0,size=X_test.shape[0])
 
-if method == 1:
+if part == 1:
 	lr = My_Linear_Regression(X_train, X_test, Y_train, lambda_)
 	lr.My_OLS()
 	energies_predict = lr.My_Predict(X_test, False)
@@ -70,7 +73,7 @@ if method == 1:
 	plt.title('$\\mathrm{OLS}$', fontsize=12)
 	plt.show()
 
-elif method == 2:
+elif part == 2:
 	lambda_ = np.array([0.001, 0.01, 0.1, 1.0])
 	coeff = np.zeros((4, np.size(X_test,1)))
 	for i in range(4):
@@ -107,11 +110,11 @@ elif method == 2:
 	plt.title('$\\mathrm{Ridge},\ \\lambda=%.4f$' %(lambda_[3]),fontsize=12)
 	plt.tick_params(labelsize=10)
 
-	plt.subplots_adjust(wspace=0.05, hspace=0.5)
+	plt.subplots_adjust(left=0.05, right=0.85, wspace=0.05, hspace=0.5)
 	plt.colorbar()
 	plt.show()
 
-elif method == 3:
+elif part == 3:
 	lambda_ = np.array([0.001, 0.01, 0.1, 1.0])
 	coeff = np.zeros((4, np.size(X_test,1)))
 	doubleR = np.zeros(4)
@@ -151,54 +154,71 @@ elif method == 3:
 	plt.title('$\\mathrm{Lasso},\ \\lambda=%.4f$' %(lambda_[3]),fontsize=12)
 	plt.tick_params(labelsize=10)
 
-	plt.subplots_adjust(wspace=0.05, hspace=0.5)
+	plt.subplots_adjust(left=0.05, right=0.85, wspace=0.05, hspace=0.5)
 	plt.colorbar()
-	#plt.show()
+	plt.show()
 
-elif method == 4:
+elif part == 4:
+	method = 'OLS' 
+	doubleR_OLS = np.zeros(9)
+	lambda_ = np.array([0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0])
+	Y_mat = np.tile(Y_test, (100, 1))
+	for i in range(9):
+		# do bootstrap 
+		boot = Bootstrap(X_train, X_test, Y_train, 100, lambda_[i], method)
+		Y_pred, coeff_train = boot.My_Bootstrap()
+		doubleR_OLS[i] = 1.0 - ((Y_mat - Y_pred)**2).sum()/((Y_mat - Y_mat.mean())**2).sum()
+
+	method = 'Ridge' 
+	doubleR_ridge = np.zeros(9)
+	for i in range(9):
+		# do bootstrap 
+		boot = Bootstrap(X_train, X_test, Y_train, 100, lambda_[i], method)
+		Y_pred, coeff_train = boot.My_Bootstrap()
+		doubleR_ridge[i] = 1.0 - ((Y_mat - Y_pred)**2).sum()/((Y_mat - Y_mat.mean())**2).sum()
+
+	method = 'Lasso' 
+	doubleR_lasso = np.zeros(9)
+	for i in range(9):
+		# do bootstrap 
+		boot = Bootstrap(X_train, X_test, Y_train, 100, lambda_[i], method)
+		Y_pred, coeff_train = boot.My_Bootstrap()
+		doubleR_lasso[i] = 1.0 - ((Y_mat - Y_pred)**2).sum()/((Y_mat - Y_mat.mean())**2).sum()
+
+	# plot
+	plt.semilogx(lambda_, doubleR_OLS, 'm--', label='OLS')
+	plt.semilogx(lambda_, doubleR_ridge, 'c--', label='Ridge')
+	plt.semilogx(lambda_, doubleR_lasso, 'g--', label='Lasso')
+	plt.title('$R^2-score$')
+	plt.xlabel('$\\lambda$')
+	plt.ylabel('Error $\mathbf{y} - \mathbf{\hat{y}}$')
+	plt.legend(loc='lower left')
+	plt.show()
+
+elif part == 5:
 	method = 'Lasso' # select the optimal method
-	#MSE = np.zeros(10)
-	#bias = np.zeros(10)
-	#variance = np.zeros(10)
-	#MSE_test = np.zeros(10)
-	#bias_test = np.zeros(10)
-	#variance_test = np.zeros(10)
-	doubleR = np.zeros(9)
+	MSE = np.zeros(9)
+	bias = np.zeros(9)
+	variance = np.zeros(9)
 	lambda_ = np.array([0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0])
 	for i in range(9):
 		# do bootstrap 
 		boot = Bootstrap(X_train, X_test, Y_train, 100, lambda_[i], method)
 		Y_pred, coeff = boot.My_Bootstrap()
-
+		#Y_mat = np.tile(Y_test, (100, 1))
 		# Calculate different statistical properties
-		#MSE[i] = np.mean(np.mean((Y_test - Y_pred)**2, axis=1, keepdims=True) )
-		#bias[i] = np.mean((Y_test - np.mean(Y_pred, axis=1, keepdims=True))**2 )
-		#variance[i] = np.mean(np.var(Y_pred, axis=1, keepdims=True) )
-
-		#MSE[i]_test = np.mean(np.mean((Y_test - Y_pred)**2, axis=1, keepdims=True) )
-		#bias[i]_test = np.mean((Y_test - np.mean(Y_pred, axis=1, keepdims=True))**2 )
-		#variance[i]_test = np.mean(np.var(Y_pred, axis=1, keepdims=True) )
-		Y_mat = np.tile(Y_test, (100, 1))
-		#doubleR[i] = 1.0 - sum((sum((Y_mat - Y_pred)**2))/sum(sum((Y_mat - np.mean(Y_mat, axis=1, keepdims=True))**2)))
-		doubleR[i] = 1.0 - ((Y_mat - Y_pred)**2).sum()/((Y_mat - Y_mat.mean())**2).sum()
-		"""
 		MSE[i] = np.mean(np.mean((-1 - coeff)**2, axis=1, keepdims=True) )
 		bias[i] = np.mean((-1 - np.mean(coeff, axis=1, keepdims=True))**2 )
 		variance[i] = np.mean(np.var(coeff, axis=1, keepdims=True) )
-		"""
+
 	# plot
-	"""
-	plt.plot(lambda_, MSE, 'r--', lambda_, bias, 'b--', lambda_, variance, 'g--')
-	plt.legend(('MSE', 'bias^2', 'variance'), loc='upper right')
-	plt.title('Bias-variance tradeoff')
+	plt.semilogx(lambda_, MSE, 'm--', label='MSE')
+	plt.semilogx(lambda_, bias, 'c--', label='$bias^2$')
+	plt.semilogx(lambda_, variance, 'g--', label='variance')
+	plt.title('Bias-variance tradeoff, ridge')
 	plt.xlabel('$\\lambda$')
-	plt.ylabel('Error')
-	plt.show()
-	"""
-	plt.plot(lambda_, doubleR, 'r--',)
-	plt.title('$R^2 score$')
-	plt.xlabel('$\\lambda$')
-	plt.ylabel('Error')
+	plt.ylabel('Error in coefficients')
+	plt.legend(loc='center left')
 	plt.show()
 
 else:
